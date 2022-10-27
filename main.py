cc74589403382f5c17ac371a00f0dd1b2b07d8fa
Janette Rocha <113930409+jrocha26@users.noreply.github.com>
2022-10-27 17:58:56 +0000

    reverse words

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 # from methods_help import method_help
-from functions import welcome, add_numbers, num_dividedFrom_add_numbers, square
-# from returnStuff import returnS
+# from functions import welcome, add_numbers, num_dividedFrom_add_numbers, square
+from returnStuff import power, usd_to_eur, reverse_word
+# # method_help()
+# #function()
+# welcome("Melanie")
+# added = add_numbers(45, 5)
+# print(added)
 
-# method_help()
-#function()
-welcome("Melanie")
-added = add_numbers(45, 5)
-print(added)
-# returnS()
-final = num_dividedFrom_add_numbers(45, added)
-print(final)
+# final = num_dividedFrom_add_numbers(45, added)
+# print(final)
+answer=power(3,5)
+print(answer)
+# square(5)
 
-square(5)
+money= usd_to_eur(3)
+print(money)
+
+word = reverse_word(python)
+print(word.reverse())
