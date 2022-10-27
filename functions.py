6b79bb29a6ef6b4774158c31bcdd6d706f5d6987
Janette Rocha <113930409+jrocha26@users.noreply.github.com>
2022-10-27 17:43:53 +0000

    used return values for functions

--- functions.py
+++ functions.py
@@ -1,8 +1,24 @@
-def function():
-  dic= {"color":red, "color" : blue}
-  dic.__reversed__()
+def welcome(name):
+  print("welcome" + name)
+
+
+def add_numbers(num1, num2):
+  print(num1,num2)
+  return num1 + num2
+
+def num_dividedFrom_add_numbers(num,addedNum):
+  return num/addedNum
+
+# def function():
+#   pass
+  
+
+  
 #   Create Functions Practice #1
   # Declare a function called greet, which every time it is called prints "Hello world!"
+
+def greet(greeting):
+  print("Hellow world!")
   
   # You should only define the function, you should not call it later.
 
@@ -13,7 +29,11 @@ def function():
 # Create Functions Practice #2
 # Declare a function called welcome, which takes a person's name as an argument, and every time it is called, it prints "Welcome {name}!"
 
+def welcome(name):
+  print("welcome" + name)
+
 # Create the variable name, and store any name inside of it, so we can test the function with a name of your choosing.
+name = welcome("Melanie")
 
 # You should only define the function and create the variable, you should not call the function afterwards.
 
@@ -22,7 +42,9 @@ def function():
 ########################################################################################################################
 # Create Functions Practice #3
 # Declare a function called square, that takes any number as an argument, and each time it is called, it prints the square of that number on the screen (that is, the value to the second power).
-
+def square(num):
+  print(num**2)
+num = square(5)
 # The name of the argument that this function must take is number. Create this variable and assign it any number.
 
 # You should only define the function and create the variable, you should not call the function afterwards.
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from methods_help import method_help
-# from functions import function
+# from methods_help import method_help
+from functions import welcome, add_numbers, num_dividedFrom_add_numbers, square
 # from returnStuff import returnS
 
-method_help()
-# function()
+# method_help()
+#function()
+welcome("Melanie")
+added = add_numbers(45, 5)
+print(added)
 # returnS()
+final = num_dividedFrom_add_numbers(45, added)
+print(final)
+
+square(5)
